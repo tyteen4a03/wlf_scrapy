3c77b45ba14b12225f20d75963a10da7be771e60
tyteen4a03 <tyteen4a03@3pointstudio.com>
2013-01-09 12:17:05 +0800

    Removed now-obsolete BOT_VERSION

--- wlf_scrapy/settings.py
+++ wlf_scrapy/settings.py
@@ -2,9 +2,8 @@
 # All rights reserved.
 
 BOT_NAME = 'wlf_scrapy'
-BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['wlf_scrapy.spiders']
 NEWSPIDER_MODULE = 'wlf_scrapy.spiders'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+USER_AGENT = '%s/%s' % BOT_NAME
 
